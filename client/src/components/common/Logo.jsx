a3011fe91f0bd1163f404642628f32477b6fa92e
mathew~ <mathewpunnen8281@gmail.com>
2025-06-26 01:30:55 +0530

    Name Change

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
 
   return (
     <Typography fontWeight="700" fontSize="1.7rem">
-Cine<span style={{color:theme.palette.primary.main}}>Flix</span>
+Film<span style={{color:theme.palette.primary.main}}>Verse</span>
     </Typography>
   )
 }
